6fbab949918e642f0d60b70799a21f1f8b4bd674
Ahmadzada-Inam <inam.ahmadzada@outlook.com>
2018-12-12 20:28:39 -0500

    added error promise

---
+++ WeatherChecker/BrokenPromise.swift
@@ -0,0 +1,9 @@
+//
+//  BrokenPromise.swift
+//  WeatherChecker
+//
+//  Created by Inam Ahmad-zada on 2018-12-12.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import Foundation
