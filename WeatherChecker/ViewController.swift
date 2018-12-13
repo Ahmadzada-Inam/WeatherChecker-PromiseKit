7dcaa73154a75f9767775902c2f966398fd85497
Ahmadzada-Inam <inam.ahmadzada@outlook.com>
2018-12-12 20:24:54 -0500

    hook up outlets

--- WeatherChecker/ViewController.swift
+++ WeatherChecker/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
+import PromiseKit
+
+private let errorColor = UIColor(red: 0.96, green: 0.667, blue: 0.690, alpha: 1.0)
+private let oneHour: TimeInterval = 3600
+private let randomCities = [("Tokyo", "JP", 35.683333, 139.683333),
+                            ("Jakarta", "ID", -6.2, 106.816667),
+                            ("Delhi", "IN", 28.61, 77.23),
+                            ("Manila", "PH", 14.58, 121),
+                            ("São Paulo", "BR", -23.55, -46.633333)]
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+    
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var randomWeatherButton: UIButton!
 }
 
