e32f32d522b20217c2a07a958d9d8e81ee041be3
Ahmadzada-Inam <inam.ahmadzada@outlook.com>
2018-12-12 19:36:17 -0500

    initial setup

---
+++ WeatherChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherChecker
+//
+//  Created by Inam Ahmad-zada on 2018-12-12.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
